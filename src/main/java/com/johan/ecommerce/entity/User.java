67f0e680af0e3fa86d0a5c34a267b47219b334fe
Johan Moreno <janmoreno@poligran.edu.co>
2026-05-15 20:34:41 -0500

    fix: handle password field securely in user entity

--- src/main/java/com/johan/ecommerce/entity/User.java
+++ src/main/java/com/johan/ecommerce/entity/User.java
@@ -1,8 +1,12 @@
 package com.johan.ecommerce.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -25,9 +29,10 @@ public class User {
     private String email;
 
     /**
-     * Se guarda encriptada y se oculta en respuestas JSON.
+     * Contraseña almacenada cifrada con BCrypt.
+     * Este campo es de solo escritura para evitar su exposición en las respuestas de la API.
      */
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     /**
